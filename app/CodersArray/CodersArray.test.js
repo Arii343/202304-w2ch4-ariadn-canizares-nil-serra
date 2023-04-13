ee844a06229b05aa1dc73fd04033f8ae6c3e10a1
nilserratio <nilserratio@gmail.com>
2023-04-13 22:33:50 +0200

    Fix the first test bugs

--- app/CodersArray/CodersArray.test.js
+++ app/CodersArray/CodersArray.test.js
@@ -1,12 +1,13 @@
 import CodersArray from "./CodersArray";
 
 describe("Given a class CodersArray that simulates six array methods and properties", () => {
-  describe("When it recive 5, 8,", () => {
-    test("Then it should return 4", () => {
-      const values = ("hello", true, 5, "bye");
-      const expectedResult = 4;
+  describe("When it recive 5, 8", () => {
+    test("Then it should return 2", () => {
+      const firstValue = 5;
+      const secondValue = 8;
+      const expectedResult = 2;
 
-      const valuesLength = new ValueLength(values);
+      const valuesLength = new CodersArray(firstValue, secondValue).length;
 
       expect(expectedResult).toBe(valuesLength);
     });
